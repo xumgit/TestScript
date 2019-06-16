5617ee8b681ed444bf91c604de844ebeaa88a164
xum <xum1281517532@163.com>
2019-06-16 09:15:39 +0800

    Add test script
    add test script, include:
    1. cypress
    2. katalon
    3. python + selenium

---
+++ python/main.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+
+import TestCase
+
+if __name__ == '__main__':
+    print("==========start Test case==========")
+    TestCase.mainFunction()
+    print("==========end Test case==========")
