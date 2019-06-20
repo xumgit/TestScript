6d935c2fa2630d0dc48cb7fb4b9a9891cb647da3
xum <xum1281517532@163.com>
2019-06-20 20:58:44 +0800

    Study cypress
    write stress test case, finished, need double test it

--- cypress/testProjectOne/cypress/fixtures/cmndFunction.js
+++ cypress/testProjectOne/cypress/fixtures/cmndFunction.js
@@ -11,6 +11,8 @@ const commonRequest = {
     body: null
 }
 
+var assignIdentifier = "10/06/2019:15:05"
+
 function Login_CMND_page() {
     cy.log("Login CMND page")
     cy.visit('/')
@@ -56,14 +58,90 @@ function Navigation_to_TVS_tv_page() {
 function Select_AllTV() {
     cy.log("Select TV")
     cy.wait(3000)
-    cy.get("#grid_devices input[name='select']")
+    cy.get("#grid_devices > thead > tr > .select-cell > .select-box")
+    .as("selectAllObj")
     .should('exist')
     .then(($targetDom) => {
         cy.wait(1000)
+        cy.log("checked:" + $targetDom.is(':checked'))
         if (!$targetDom.is(':checked')) {
-            $targetDom.click()
+            cy.get('@selectAllObj').click()
+            cy.wait(3000)
         }
     })
 }
 
-module.exports = {Login_CMND_page, Navigation_to_TVS_tv_page, Select_AllTV}
+function Assign_Clone_data(tvData, selectCloneType, selectCloneName) {
+    cy.wait(3000)
+    cy.get("#assign_select")
+    .click()
+    .then(($dom) => {
+        cy.wait(1000)
+        let liLen = $dom.find("ul>li").length
+        for (let i = 0; i < liLen; i++) {
+            let cloneTypeObj = $dom.find("ul>li:eq(" + i + ")")
+            let cloneTypeText = cloneTypeObj.text()
+            if (null != cloneTypeText && cloneTypeText.indexOf(selectCloneType) > -1) {
+                cloneTypeObj.click()
+                break
+            }
+        }
+        //$dom.find("ul li:eq(5)").click() // click channels
+        cy.wait(3000)
+        cy.get("#assignSelectRows > tr")
+        //.its('length')
+        //.should('be.gt', 1)
+        //.contains('CypressTestCloneData')
+        //.click()
+        .each(($tr, index, $arrList) => {
+            let selectText = $tr.find("td").eq(0).text()
+            //cy.log("text_" + index + ":" + selectText)
+            if (selectText.indexOf(selectCloneName) > -1) {
+                //cy.log("CloneData select success")
+                $tr.click()                              
+            }
+        })
+        .then(() => {
+            cy.wait(10000)
+            cy.get("#tvsBody tr:eq(0)")
+            .find("#tv_CloneDiv")
+            .should('have.css', 'color', tvData.BlueColor)
+            .then(($tvCloneDiv) => {
+                let assignCloneIdentifier = $tvCloneDiv.attr("sicloneident")
+                let IdentifierArr = assignCloneIdentifier.split("-")
+                let yeatMonthDay = IdentifierArr[0].split("/")
+                let hourMinutesSeconds = IdentifierArr[1].split(":")
+                assignIdentifier = "\"" + yeatMonthDay[2] + "/" + yeatMonthDay[1] + "/" + yeatMonthDay[0] 
+                                    + ":" + hourMinutesSeconds[0] + ":" + hourMinutesSeconds[1] + "\"" 
+            })
+        })
+    })
+    return assignIdentifier
+}
+
+function Click_Force_Upgrade_button() {
+    cy.get("#allBtn")
+    .should('be.visible')
+    .contains('Force')
+    .click()
+}
+
+function checkCloneColor(logType, targetColor) {    
+    cy.wait(3000)
+    var allSame = true
+    cy.get("#tvsBody > tr")
+    .each(($tr, index, $arrList) => {
+        let trColor = $tr.find("#tv_CloneDiv").css("color")
+        //cy.log(trColor + "," + targetColor)
+        if (trColor != targetColor) {
+            allSame = false
+        }
+    })
+    if (allSame) {
+        cy.log(logType + ", check color success!")
+    } else {
+        cy.log(logType + ", check color failed!")
+    }
+}
+
+module.exports = {Login_CMND_page, Navigation_to_TVS_tv_page, Select_AllTV, Assign_Clone_data, Click_Force_Upgrade_button, checkCloneColor}
--- cypress/testProjectOne/cypress/integration/test/StressTest.js
+++ cypress/testProjectOne/cypress/integration/test/StressTest.js
@@ -1,39 +1,43 @@
 const {generateTVDiscovery, generateReadyForUpgrade, generateUpGradeInProgress, generateNotInUpgradeMode} = require('../../fixtures/commonFunction.js')
-const {Login_CMND_page, Navigation_to_TVS_tv_page, Select_AllTV} = require('../../fixtures/cmndFunction.js')
-var generateTvs = 5
+const {Login_CMND_page, Navigation_to_TVS_tv_page, Select_AllTV, Assign_Clone_data, Click_Force_Upgrade_button, checkCloneColor} = require('../../fixtures/cmndFunction.js')
 
+var generateTvs = 3
+const selectCloneType = "Channels"
+const selectCloneName = "CypressTestCloneData"
+let assignIdentifier = "10/06/2019:15:05"
 
 
 context('Stress Test', () => {
+   
     beforeEach(function() {
-	    cy.log('beforeEach => runs before each test in the block')
-        cy.fixture('tvdata.json').as('TVData')
+	    cy.log('beforeEach function')
+        cy.fixture('tvdata.json').as('TVData') 
     })
 
-    it('Open CMND Page and Login', () => {
-        //Login_CMND_page();
-        //cy.wait(2000);
-        //Navigation_to_TVS_tv_page();
-    })
-
-    it('Genarate Many TVs', () => {
-        cy.wait(2000);
-        cy.get('@TVData').then((tvData) => {
-            let changeCloneItem = {};
-            changeCloneItem[tvData.CloneItems["TVChannelList"]] = "01/06/2019:08:05";
+    it('Start', () => { 
+        cy.get('@TVData').then((tvData) => { 
+            Login_CMND_page();     
+            cy.wait(2000);
+            Navigation_to_TVS_tv_page();
+            cy.wait(2000);                   
             generateTVDiscovery(tvData, generateTvs);
-        })
-    })
-
-    it('Send ReadForUpgrade for display SW version', () => {
-        cy.wait(2000);
-        cy.get('@TVData').then((tvData) => {
+            cy.wait(2000);
             generateReadyForUpgrade(tvData, generateTvs);
+            cy.wait(2000);
+            Select_AllTV();
+            assignIdentifier = Assign_Clone_data(tvData, selectCloneType, selectCloneName);
+            cy.wait(3000);
+            checkCloneColor("AssignClone", tvData.BlueColor);
+            Click_Force_Upgrade_button();
+            generateUpGradeInProgress(tvData, generateTvs);
+            cy.wait(3000);
+            checkCloneColor("UpGradeInProgress", tvData.OrangeColor);
+            cy.wait(10000);
+            let changeCloneItem = {};
+            changeCloneItem[tvData.CloneItems["TVChannelList"]] = assignIdentifier;
+            generateNotInUpgradeMode(tvData, generateTvs, changeCloneItem);
+            cy.wait(3000);
+            checkCloneColor("NotInUpgradeMode", tvData.GreenColor);
         })
-    })
-
-    it('Select AllTV', () => {
-        //cy.wait(2000);
-        //Select_AllTV();
     })
 })
