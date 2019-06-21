e206bd104c1ff9fdf76b641eb40d43eb5f9b6a32
xum <xum1281517532@163.com>
2019-06-21 20:18:22 +0800

    study cypress
    stress test finished code

--- cypress/testProjectOne/cypress/integration/test/StressTest.js
+++ cypress/testProjectOne/cypress/integration/test/StressTest.js
@@ -4,7 +4,7 @@ const {Login_CMND_page, Navigation_to_TVS_tv_page, Select_AllTV, Assign_Clone_da
 var generateTvs = 3
 const selectCloneType = "Channels"
 const selectCloneName = "CypressTestCloneData"
-let assignIdentifier = "10/06/2019:15:05"
+var assignIdentifier = "10/06/2019:15:05"
 
 
 context('Stress Test', () => {
@@ -15,29 +15,41 @@ context('Stress Test', () => {
     })
 
     it('Start', () => { 
+        Login_CMND_page();     
+        cy.wait(2000);
+        Navigation_to_TVS_tv_page();
+        cy.wait(2000);
+        cy.get('@TVData').then((tvData) => {                               
+            generateTVDiscovery(tvData, generateTvs);           
+        })
+        cy.wait(2000);
         cy.get('@TVData').then((tvData) => { 
-            Login_CMND_page();     
-            cy.wait(2000);
-            Navigation_to_TVS_tv_page();
-            cy.wait(2000);                   
-            generateTVDiscovery(tvData, generateTvs);
-            cy.wait(2000);
             generateReadyForUpgrade(tvData, generateTvs);
-            cy.wait(2000);
-            Select_AllTV();
+        })
+        cy.wait(2000);
+        Select_AllTV();
+        cy.get('@TVData').then((tvData) => {            
             assignIdentifier = Assign_Clone_data(tvData, selectCloneType, selectCloneName);
             cy.wait(3000);
             checkCloneColor("AssignClone", tvData.BlueColor);
-            Click_Force_Upgrade_button();
+        })
+        cy.wait(1000);  
+        Click_Force_Upgrade_button();
+        cy.wait(1000);
+        cy.get('@TVData').then((tvData) => { 
             generateUpGradeInProgress(tvData, generateTvs);
             cy.wait(3000);
             checkCloneColor("UpGradeInProgress", tvData.OrangeColor);
-            cy.wait(10000);
+        })           
+        cy.wait(5000);
+        cy.get('@TVData').then((tvData) => { 
             let changeCloneItem = {};
             changeCloneItem[tvData.CloneItems["TVChannelList"]] = assignIdentifier;
+            //cy.log("assignIdentifier:" + JSON.stringify(changeCloneItem))
             generateNotInUpgradeMode(tvData, generateTvs, changeCloneItem);
             cy.wait(3000);
             checkCloneColor("NotInUpgradeMode", tvData.GreenColor);
-        })
+            cy.wait(10000);
+        })         
     })
 })
