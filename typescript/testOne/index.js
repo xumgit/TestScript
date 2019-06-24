15abe38510f905aa0d1d5b58bb38dbee19c8d76e
meng.xu <xum1281517532@163.com>
2019-06-24 17:06:18 +0800

    study typescript, need continue

--- typescript/testOne/index.js
+++ typescript/testOne/index.js
@@ -1,6 +1,99 @@
 console.log("Start Type Script");
 var message = "Hello World";
 console.log(message);
+class Site {
+    name() {
+        console.log("Hello Site");
+    }
+}
+var obj = new Site();
+obj.name();
+let num = 11;
+let str = "testStr";
+let flag = true;
+let arr = [1, 2, 3, 4, 5];
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["green"] = 1] = "green";
+    Color[Color["blue"] = 2] = "blue";
+})(Color || (Color = {}));
+;
+let c = Color.green;
+if (num > 100) {
+    console.log("greate than 100");
+}
+else if (num > 50) {
+    console.log("greate than 50");
+}
+else if (num > 10) {
+    console.log("greate than 10");
+}
+else if (num > 0) {
+    console.log("greate than 0");
+}
+switch (str) {
+    case "A": {
+        console.log("A");
+        break;
+    }
+    case "B": {
+        console.log("B");
+        break;
+    }
+    case "C": {
+        console.log("C");
+        break;
+    }
+    case "testStr": {
+        console.log("testStr");
+        break;
+    }
+    default: {
+        console.log("default");
+        break;
+    }
+}
+var i;
+var total = 5;
+var sum = 0;
+for (i = 0; i < total; i++) {
+    sum += i;
+}
+console.log("sum:" + sum);
+arr.forEach((val, index, srcArray) => {
+    console.log(val + "|" + index + "|(" + srcArray + ")");
+});
+function greet() {
+    return "Hello Greet";
+}
+console.log("result:" + greet());
+function add(x, y) {
+    return x + y;
+}
+console.log("result:" + add(1, 2));
+function buildName(firstName, lastName) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
+let result1 = buildName("Bob");
+let result2 = buildName("Bob", "Adams");
+console.log("result1:" + result1 + ",result2:" + result2);
+function calculate_discount(price, rate = 0.50) {
+    var discount = price * rate;
+    console.log("result:", discount);
+}
+calculate_discount(1000);
+calculate_discount(1000, 0.30);
+var foo = (x) => 10 + x;
+console.log(foo(100));
+var foo1 = (x) => {
+    x = 10 + x;
+    console.log(x);
+};
+foo1(100);
 // // 1. Set
 // let set1 = new Set(['red', 'green', 'blue']);
 // for (let item of set1.keys()) {
--- typescript/testOne/index.ts
+++ typescript/testOne/index.ts
@@ -3,6 +3,101 @@ console.log("Start Type Script")
 var message:string = "Hello World" 
 console.log(message)
 
+class Site {
+    name(): void {
+        console.log("Hello Site");
+    }
+}
+
+var obj = new Site();
+obj.name();
+
+let num: number = 11;
+let str: string = "testStr";
+let flag: boolean = true;
+let arr: number[] = [1, 2, 3, 4, 5];
+enum Color {red, green, blue};
+let c: Color = Color.green;
+
+if (num > 100) {
+    console.log("greate than 100");
+} else if (num > 50) {
+    console.log("greate than 50");
+} else if (num > 10) {
+    console.log("greate than 10");
+} else if (num > 0) {
+    console.log("greate than 0");
+}
+
+switch (str) {
+    case "A": {
+        console.log("A");
+        break;
+    }
+    case "B": {
+        console.log("B");
+        break;
+    }
+    case "C": {
+        console.log("C");
+        break;
+    }
+    case "testStr": {
+        console.log("testStr");
+        break;
+    }
+    default: {
+        console.log("default");
+        break;
+    }
+}
+
+var i:number;
+var total:number = 5;
+var sum:number = 0;
+for (i = 0; i < total; i++) {
+    sum += i;
+}
+console.log("sum:" + sum);
+
+arr.forEach((val, index, srcArray) => {
+    console.log(val + "|" + index + "|(" + srcArray + ")");
+});
+
+function greet():string {
+    return "Hello Greet";
+}
+console.log("result:" + greet());
+
+function add(x: number, y: number): number {
+    return x + y;
+}
+console.log("result:" + add(1, 2));
+
+function buildName(firstName: string, lastName?: string) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
+let result1 = buildName("Bob"); 
+let result2 = buildName("Bob", "Adams"); 
+console.log("result1:" + result1 + ",result2:" + result2);
+
+function calculate_discount(price:number,rate:number = 0.50) { 
+    var discount = price * rate; 
+    console.log("result:",discount); 
+} 
+calculate_discount(1000);
+calculate_discount(1000,0.30);
+
+var foo = (x:number)=>10 + x 
+console.log(foo(100));
+var foo1 = (x:number)=> {    
+    x = 10 + x; 
+    console.log(x);  
+} 
+foo1(100);
 // // 1. Set
 // let set1 = new Set(['red', 'green', 'blue']);
 
